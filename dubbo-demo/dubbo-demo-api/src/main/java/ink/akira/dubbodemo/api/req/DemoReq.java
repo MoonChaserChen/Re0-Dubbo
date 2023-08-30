5fa8b8338a2bc35029b5533955b2c654bd13c393
Akira <chin.kou.akira@gmail.com>
2023-08-31 03:54:53 +0800

    no commit message

--- dubbo-demo/dubbo-demo-api/src/main/java/ink/akira/dubbodemo/api/req/DemoReq.java
+++ dubbo-demo/dubbo-demo-api/src/main/java/ink/akira/dubbodemo/api/req/DemoReq.java
@@ -1,13 +1,10 @@
 package ink.akira.dubbodemo.api.req;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DemoReq {
     private int id;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 }
--- dubbo-demo/dubbo-demo-api/src/main/java/ink/akira/dubbodemo/api/resp/DemoResp.java
+++ dubbo-demo/dubbo-demo-api/src/main/java/ink/akira/dubbodemo/api/resp/DemoResp.java
@@ -1,31 +1,12 @@
 package ink.akira.dubbodemo.api.resp;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DemoResp {
     private int id;
     private String name;
     private int age;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
